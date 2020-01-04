bb6885552430e9ed976c90baf6363a7ba21935fc
Maksym Bobryshev <maksym.bobmm@gmail.com>
2020-01-04 14:51:39 +0200

    Works points on map
    Add fill polygon
    Add gem jquery-rails

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,6 +10,52 @@ class TicketsController < ApplicationController
     head 422 and return unless params['id']
 
     @ticket = Ticket.includes(:excavator).find(params['id'])
+    geojson = make_geojson(@ticket.digsite_info['WellKnownText'])
+    gon.geojson = geojson
   end
   # rubocop:enable Style/AndOr
+
+  private
+
+  def make_geojson(ticket)
+    geojson = receive_json_template
+    coordinates = parse_coordinates(ticket)
+    coordinates.each do |coordinate|
+      geojson[:source][:data][:features] << {
+                                              type: 'Feature',
+                                              geometry: {
+                                                type: 'Point',
+                                                coordinates: coordinate
+                                              }
+                                            }
+    end
+    geojson
+  end
+
+  def parse_coordinates(ticket)
+    str = ticket.delete!(')' + 'POLYGON((')
+    str.split(',').map { |element| element.split(' ').map(&:to_d) }
+  end
+
+  def receive_json_template
+    {
+      id: 'points',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: []
+        }
+      },
+      layout: {
+          'icon-image': 'exc',
+          'icon-size': 0.09,
+      },
+      paint: {
+        'fill-color': '#369',
+        'fill-opacity': 0.8
+      }
+    }
+  end
 end
